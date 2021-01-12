eba1a2c1cb6766ed1b700ec916a26b8d9fc7b60e
PavelProst <pavelprost@yahoo.com>
2021-01-12 23:29:14 +0200

    three commit

--- script.js
+++ script.js
@@ -9,41 +9,54 @@ const model = [
     },
     {
         type: 'columns',
-        value: ['1111111', '2222222', '333333333'],
+        value: ['1111111', '2222222', '333333333', '444444'],
+    },
+    {
+        type: 'image', value: './assets/slider1.png',
     },
 ]
 
 const $site = document.querySelector('#site');
 
 model.forEach(block => {
-        let html = '';
+    let html = '';
 
-        if (block.type === 'title') {
-            html = `
-            <div class="row">
+    if (block.type === 'title') {
+        html = title(block);
+    } else if (block.type === 'text') {
+        html = text(block);
+    } else if (block.type === 'columns') {
+        html = columns(block);
+    } else if (block.type === 'image') {
+        html = image(block);
+    }
+    $site.insertAdjacentHTML('beforeend', html);
+
+});
+
+function title(block) {
+    return `<div class="row">
             <div class="col-sm">
                 <h1>${block.value}</h1>
             </div>
-            </div>
-        `
-        } else if (block.type === 'text') {
-            html = `<div class="row">
+        </div>
+       `
+}
+
+function text(block) {
+    return `<div class="row">
             <div class="col-sm">
                 <p>${block.value}</p>
             </div>
         </div>
         `
-        } else if (block.type === 'columns') {
-            html = `<div class="row">`
-            for (let i = 0; i < block.value.length; i++) {
-            html += `<div class="col-sm">
-                ${block.value[i]}
-            </div>`
-            }
-            html += `</div>`
-        }
+}
 
-        $site.insertAdjacentHTML('beforeend', html);
+function columns(block) {
+    const html = block.value.map(item => `<div class="col-sm">${item}</div>`)
+    return `<div class="row">${html.join('')}</div>`
+}
 
-    });
-
+function image(block) {
+    return `<div class="row"><div class="col-sm"><img src="${block.value}" width="100px" height="100px"/></div></div>`
+}
